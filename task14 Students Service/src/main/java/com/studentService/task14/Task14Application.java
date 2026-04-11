69013e257c19c352ee3bf6354a1321f891cea9b7
JenithaReniSharan24504 <vtu24504@veltech.edu.in>
2026-04-12 00:54:29 +0530

    Add Eureka client registration and startup messages

--- task14 Students Service/src/main/java/com/studentService/task14/Task14Application.java
+++ task14 Students Service/src/main/java/com/studentService/task14/Task14Application.java
@@ -6,10 +6,13 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class Task14Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Task14Application.class, args);
+		System.out.println("Student Service started on port 8081");
+		System.out.println("Registered with Eureka at http://localhost:8761");
 	}
 
 }
